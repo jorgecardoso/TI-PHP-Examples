62b80bc59f0b7f02190cfbb452545fc34f0e4845
Jorge C S. Cardoso <jorgecardoso@ieee.org>
2020-12-04 16:08:27 +0000

    Cleanup up desktop.ini files Created example index

--- index.php
+++ index.php
@@ -1,2 +1,40 @@
-<?php
-echo "Hello, world!";
+<html>
+<head>
+</head>
+<body>
+<ul>
+
+  <li><a href="01-phpfile/">Example 1. Including PHP script in HTML</a></li>
+
+  <li><a href="02-writingcontent/">Example 2. Writing content to the HTML</a></li>
+
+  <li><a href="03-variables/">Example 3. Variables in PHP</a></li>
+
+  <li><a href="04-conditions/">Example 4. Conditions in PHP</a></li>
+
+  <li><a href="05-forms/">Example 5. Forms</a></li>
+
+  <li><a href="06-writingfile/">Example 6. Writing to file on server</a></li>
+
+  <li><a href="07-writingfile-form/">Example 7. Writing form data to file on server</a></li>
+
+  <li><a href="08-including/">Example 8. Including other files</a></li>
+
+  <li><a href="09-arrays/">Example 9. Manipulating arrays in PHP</a></li>
+
+  <li><a href="10-conditions-html/">Example 10. HTML in PHP conditions</a></li>
+
+  <li><a href="11-functions/">Example 11. Functions in PHP</a></li>
+
+  <li><a href="12-mail/">Example 12. Sending mail from PHP</a></li>
+
+  <li><a href="13-csv/">Example 13. Read and write CSV (Comma Separated Values) data</a></li>
+
+  <li><a href="14-fileupload/">Example 14. Upload files</a></li>
+
+  <li><a href="15-session/">Example 15. User sessions</a></li>
+
+</ul>
+</body>
+
+</html>
